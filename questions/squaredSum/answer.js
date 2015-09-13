4a0afa66f8dc7bc1c46e7dde892c0befeeda828c
audibleblink <virtualsnyper@gmail.com>
2015-09-13 11:04:14 -0400

    add squared sums question

---
+++ questions/squaredSum/answer.js
@@ -0,0 +1,33 @@
+
+function sumSquared(number) {
+  var factors = factorsFor(number)
+  return factors
+    .map(function(el) {
+      // returns an array with all elements of factors, squared
+      return el * el
+    })
+    .reduce(function(memo, el) {
+      // returns the memo at the end of the iteration
+      return memo + el
+    })
+}
+
+function factorsFor(num) {
+  var factors = [];
+  for (var pf = 0; pf < Math.sqrt(num); pf++) {
+    if (num % pf == 0) {
+      // If we can find a lower factor, we can infer its
+      // inverse factor by dividing num by the lower factor.
+      // Because of this, we only ever need to iterate upto
+      // the square root of n, making this O(log n)
+      factors.push(pf)
+      factors.push(num / pf)
+    }
+  }
+  return factors
+}
+
+console.log(
+  "Returns a number's factors, squared and summed:\n",
+  sumSquared(12) === 210
+);
---
+++ questions/squaredSum/readme.md
@@ -0,0 +1,26 @@
+## Squared Sum
+
+The number 12 has the factors 1, 2, 3, 4, 6, and 12.
+
+Their squares are 1,4,9,16,36,144.
+
+If you add them all up, you get 210
+
+Write a function that can take any integer, find its facors, then square them and add them. Return the sum.
+
+
+### Function Signature
+
+```js
+sumSquared(number:Number) -> Number
+```
+
+### Examples
+
+```ruby
+sumSquared(12) -> 210
+```
+
+### Constraints
+
+- The finding of a number's factors can be done in O(log n)
